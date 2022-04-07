9bd417e91c20a2c916f8de0be42c2dd5cf10ab24
lutheranders1 <lutheranders@gmail.com>
2022-04-07 00:45:50 -0500

    comittt

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 //import './styles/main.scss'
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
 ReactDOM.render(
   <Router>
