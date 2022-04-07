adaaa70772221e151d1802374ad6797ddd2d48e3
lutheranders1 <lutheranders@gmail.com>
2022-04-06 23:39:50 -0500

    deploy

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import { port, dbURI } from './config/environment.js'
-import router from './config/router.js'
-import path from 'path'
+import express from "express";
+import mongoose from "mongoose";
+import { port, dbURI } from "./config/environment.js";
+import router from "./config/router.js";
+import path from "path";
 
-
-const app = express()
-const __dirname = path.resolve() // added for deployment
+const app = express();
+const __dirname = path.resolve(); // added for deployment
 
 const startServers = async () => {
   try {
-    await mongoose.connect(dbURI)
-    console.log('Database connected successfully')
+    await mongoose.connect(dbURI);
+    console.log("Database connected successfully");
 
     //JSON Parser
-    app.use(express.static(`${__dirname}/front-end/build`)) // deployment, path which defines where data is served from
-    app.use(express.json())
+    app.use(express.static(`${__dirname}/frontend/build`)); // deployment, path which defines where data is served from
+    app.use(express.json());
 
     //Logger
     // app.use((req, _res, next) => {
@@ -25,21 +24,25 @@ const startServers = async () => {
 
     //Router
 
-    app.use('/api', router)
+    app.use("/api", router);
 
     // catcher middleware
-    app.use('/*', (_, res) => res.sendFile(`${__dirname}/front-end/build/index.html`))
+    app.use("/*", (_, res) =>
+      res.sendFile(`${__dirname}/frontend/build/index.html`)
+    );
 
     //Catch all
     app.use((_req, res) => {
-      res.status(404).json({ message: `Route not found` })
-    })
+      res.status(404).json({ message: `Route not found` });
+    });
 
     //Staring our server once our db has successfully connected
-    app.listen(port, () => console.log(`Server up and running on port ${port}`))
+    app.listen(port, () =>
+      console.log(`Server up and running on port ${port}`)
+    );
   } catch (err) {
-    console.log(`ERROR: Something has gone wrong`)
-    console.log(err)
+    console.log(`ERROR: Something has gone wrong`);
+    console.log(err);
   }
-}
-startServers()
+};
+startServers();
