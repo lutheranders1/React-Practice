09dc61425acea2b7ebb49b7e2c7e1a59e3baf542
lutheranders1 <lutheranders@gmail.com>
2022-04-07 03:31:41 -0500

    commit

--- config/environment.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const dbURI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://lutheranders:k83khC5QmMp9twf@cluster0.uebgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-export const port = process.env.PORT || 4000;
-export const secret = process.env.SECRET || "secret";
